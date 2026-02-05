e2f0837ed524cb9a41c5f5fa4fa418a2fffed986
balaj <balaj@KingsMan>
2026-02-05 12:04:15 +0530

    this is launchBrowser prject

---
+++ JavaSelinumProject/src/DemoPackage/LaunchChromeBrowser.java
@@ -0,0 +1,11 @@
+package DemoPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+	public static void main(String[] args) throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	Thread.sleep(500);
+	driver.close();
+	}
+}
